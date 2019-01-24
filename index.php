43e83f7c1dcc573324964f47d48610eec70d5b2a
Felipe Lopes <felipelm07@gmail.com>
2019-01-24 20:55:00 -0200

    felipe
    ajuste

--- index.php
+++ index.php
@@ -5,13 +5,20 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+    <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="Content-Language" content="pt-br">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="copyright" content="Accenture Brasil">
+            <meta name="author" content="felipe.lopes.melo@accenture.com">
+            <meta name="robots" content="noindex, nofollow">
+            <link rel="shortcut icon" href="https://rentadrone.cl/wp-content/uploads/2016/12/pie-chart-1.png" />
+            <title>Pool ABAP</title>
+        </head>
+        <body>
+            <?php
+            // put your code here
+            ?>
+        </body>
+    </html>
